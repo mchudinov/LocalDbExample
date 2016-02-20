465f85fee5dda3b7fd4084c35be2ddd14cec97b3
Mikael Chudinov <mikael@chudinov.net>
2016-02-20 17:42:06 +0100

    Initial commit.

---
+++ LocalDbExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LocalDbExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LocalDb Example");
+            var constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+            //INSERT
+            using (var con = new SqlConnection(constr))
+            {
+                con.Open();
+                    var query = new SqlCommand($"INSERT INTO [Table] (Sum) VALUES ({new Random().Next(100)})", con);
+                    query.ExecuteNonQuery();
+                con.Close();
+            }
+
+            //SELECT
+            using (var con = new SqlConnection(constr))
+            {
+                con.Open();
+                    var query = new SqlCommand($"SELECT * FROM [Table]", con);
+                    using (var reader = query.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.Write($"Id={reader["Id"]} \t");
+                            Console.WriteLine($"Sum={reader["Sum"]}");
+                        }
+                    }
+                con.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
